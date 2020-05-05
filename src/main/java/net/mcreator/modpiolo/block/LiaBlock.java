0ed251fade24331c70245384d0df46d47dea6397
lizar <lizar@DESKTOP-O95ULBO>
2020-05-05 01:46:26 -0300

    Tercer commit

---
+++ src/main/java/net/mcreator/modpiolo/block/LiaBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.modpiolo.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.modpiolo.ModpioloElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@ModpioloElements.ModElement.Tag
+public class LiaBlock extends ModpioloElements.ModElement {
+	@ObjectHolder("modpiolo:lia")
+	public static final Block block = null;
+	public LiaBlock(ModpioloElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("lia");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
